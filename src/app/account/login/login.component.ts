5e7e3eff1cb9cc14d3fd8b589de0e0859c2603fc
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-08-05 00:45:48 -0300

    import formmodule, httpclientmodule in modules, create layout create account and login, create fake functions to create a token and put in sessionstorage and redirect to home

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,4 +1,6 @@
+import { AccountService } from './../shared/account.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  login = {
+    email: '',
+    password: ''
+  };
+
+  constructor(private accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  async onSubmit() {
+    try {
+      const result = await this.accountService.login(this.login);
+      this.router.navigate(['']);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
 }
