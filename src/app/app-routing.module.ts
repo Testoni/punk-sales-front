2beaa00eeb8809ff184e1b50bfa394637883d03c
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-08-05 00:24:06 -0300

    configurate route to watch AuthGuard, only acess if contains token in storage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { AuthGuard } from './account/shared/auth.guard';
+import { CreateAccountComponent } from './account/create-account/create-account.component';
+import { LoginComponent } from './account/login/login.component';
 import { HomeComponent } from './layout/home/home.component';
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthenticationComponent } from './layout/authentication/authentication.component';
 
 const routes: Routes = [
     {
@@ -12,8 +16,28 @@ const routes: Routes = [
                 path: '',
                 component: AppComponent
             }
-        ]
+        ],
+        canActivate: [AuthGuard]
     },
+    {
+        path: '',
+        component: AuthenticationComponent,
+        children: [
+            {
+                path: '',
+                redirectTo: 'login',
+                pathMatch: 'full'
+            },
+            {
+                path: 'login',
+                component: LoginComponent
+            },
+            {
+                path: 'create-account',
+                component: CreateAccountComponent
+            }
+        ]
+    }
 ];
 
 @NgModule({
